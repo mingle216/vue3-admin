5ce0b61e6aad896355938b25eef85d87e5ecde25
mingle216 <hemingming666@gmail.com>
2022-05-04 22:45:44 +0800

    feat: 获取用户基本信息

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,16 +1,22 @@
-import { login } from '@/api/sys'
+import { login, getUserInfo } from '@/api/sys'
 import md5 from 'md5'
 import { setItem, getItem } from '@/utils/storage'
 import { TOKEN } from '@/constant'
 export default {
   namespaced: true,
   state: () => ({
-    token: getItem(TOKEN) || ''
+    token: getItem(TOKEN) || '',
+    // 增加
+    userInfo: {}
   }),
   mutations: {
     setToken(state, token) {
       state.token = token
       setItem(TOKEN, token)
+    },
+    // 增加
+    setUserInfo(state, userInfo) {
+      state.userInfo = userInfo
     }
   },
   actions: {
@@ -31,6 +37,13 @@ export default {
             reject(err)
           })
       })
+    },
+    // 增加
+    async getUserInfo(context) {
+      const res = await getUserInfo()
+      console.log(res)
+      this.commit('user/setUserInfo', res)
+      return res
     }
   }
 }
