9a854b858c05e69552a515f06b0999de5d249358
mingle216 <hemingming666@gmail.com>
2022-05-04 22:50:59 +0800

    feat: 退出登录方案实现

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,6 +1,9 @@
 import { login, getUserInfo } from '@/api/sys'
 import md5 from 'md5'
-import { setItem, getItem } from '@/utils/storage'
+// 增加解构removeAllItem
+import { setItem, getItem, removeAllItem } from '@/utils/storage'
+// 增加 导入路由
+import router from '@/router'
 import { TOKEN } from '@/constant'
 export default {
   namespaced: true,
@@ -44,6 +47,12 @@ export default {
       console.log(res)
       this.commit('user/setUserInfo', res)
       return res
+    },
+    logout() {
+      this.commit('user/setToken', '')
+      this.commit('user/setUserInfo', {})
+      removeAllItem()
+      router.push('/login')
     }
   }
 }
