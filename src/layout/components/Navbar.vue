21a152f0012082a4401730562965a99f0ba9f647
mingle216 <hemingming666@gmail.com>
2022-05-05 00:23:12 +0800

    feat: 封装LangSelect组件

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -3,6 +3,7 @@
     <hamburger class="hamburger-container" />
     <breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
+      <lang-select class="right-menu-item hover-effect" />
       <!-- 头像 -->
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
@@ -28,6 +29,7 @@
 <script setup>
 import Hamburger from '@/components/Hamburger.vue'
 import Breadcrumb from '@/components/Breadcrumb'
+import LangSelect from '@/components/LangSelect'
 import { useStore } from 'vuex'
 import { ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
@@ -85,6 +87,19 @@ watch(
     align-items: center;
     float: right;
     padding-right: 16px;
+
+    ::v-deep(.right-menu-item) {
+      display: inline-block;
+      padding: 0 18px 0 0;
+      font-size: 24px;
+      color: #5a5e66;
+      vertical-align: text-bottom;
+      margin-left: 8px;
+
+      &.hover-effect {
+        cursor: pointer;
+      }
+    }
 
     ::v-deep(avatar-container) {
       cursor: pointer;
