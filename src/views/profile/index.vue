9e37b3689ed494a43797771f2c1cd36f49785756
mingle216 <hemingming666@gmail.com>
2022-05-06 19:54:01 +0800

    feat: 项目介绍模块开发

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -2,7 +2,7 @@
   <div class="my-container">
     <el-row>
       <el-col :span="6">
-        <project-card class="user-card"></project-card>
+        <project-card class="user-card" :features="featureData"></project-card>
       </el-col>
       <el-col :span="18">
         <el-card>
@@ -24,12 +24,18 @@
 </template>
 
 <script setup>
+import { getFeature } from '@/api/user'
 import ProjectCard from './components/ProjectCard.vue'
 import Chapter from './components/Chapter.vue'
 import Feature from './components/Feature.vue'
 import Author from './components/Author.vue'
 import { ref } from 'vue'
 const activeName = ref('feature')
+const featureData = ref([])
+const getFeatureData = async () => {
+  featureData.value = await getFeature()
+}
+getFeatureData()
 </script>
 
 <style lang="scss" scoped>
