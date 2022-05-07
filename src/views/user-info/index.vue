8f9fe56f2bf1b62ac8d6a3f1ced49dbe06b16db1
mingle216 <hemingming666@gmail.com>
2022-05-07 23:46:57 +0800

    feat: 获取展示数据

---
+++ src/views/user-info/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { userDetail } from '@/api/user-manage'
+import { watchSwitchLang } from '@/utils/i18n'
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+  id: {
+    type: String,
+    required: true
+  }
+})
+
+// 数据相关
+const detailData = ref({})
+const getUserDetail = async () => {
+  detailData.value = await userDetail(props.id)
+}
+getUserDetail()
+// 语言切换
+watchSwitchLang(getUserDetail)
+</script>
