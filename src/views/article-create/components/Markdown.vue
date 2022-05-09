8febaa82679094cd0ecdace6c804f5c47343d983
mingle216 <hemingming666@gmail.com>
2022-05-09 21:31:15 +0800

    feat: 新建文章:markdown文章提交

--- src/views/article-create/components/Markdown.vue
+++ src/views/article-create/components/Markdown.vue
@@ -10,14 +10,35 @@
 
 <script setup>
 import {} from 'vue'
+import { commitArticle } from './commit'
 import { watchSwitchLang } from '@/utils/i18n'
-watchSwitchLang(() => {
-  if (!el) return
-  const htmlStr = mkEditor.getHTML()
-  mkEditor.destroy()
-  initEditor()
-  mkEditor.setHTML(htmlStr)
+const props = defineProps({
+  title: {
+    required: true,
+    type: String
+  }
 })
+
+const emits = defineEmits(['onSuccess'])
+// 处理提交
+const onSubmitClick = async () => {
+  if (props.detail && props.detail._id) {
+    // 编辑文章
+    await editArticle({
+      id: props.detail._id,
+      title: props.title,
+      content: mkEditor.getHTML()
+    })
+  } else {
+    // 创建文章
+    await commitArticle({
+      title: props.title,
+      content: mkEditor.getHTML()
+    })
+  }
+  mkEditor.reset()
+  emits('onSuccess')
+}
 </script>
 
 <style lang="scss" scoped>
